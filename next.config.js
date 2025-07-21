4e1a363ab9a74bcc582af3d78d5fd81ea38305df
Nuh Mh <shifteher@gmail.com>
2025-07-21 11:49:12 +0000

    Error: Invalid src prop (https://www.bing.com/ck/a?!&&p=4c1467e6f3261081

--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.bing.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
